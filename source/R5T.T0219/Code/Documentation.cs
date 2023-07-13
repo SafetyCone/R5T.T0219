ca855d77a3d172103cc946226bc7be4ae86cf30f
davidcoats <david@getsafetycone.com>
2023-07-13 00:12:39 -0600

    Initial commit.

---
+++ source/R5T.T0219/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0219
+{
+	/// <summary>
+	/// .NET (dotnet) version and other related strong types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
